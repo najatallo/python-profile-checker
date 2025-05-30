7e5d1201c496e1afa3fde0c2648cebdab6165f5e
Najat Allo <najatallo21@gmail.com>
2025-05-30 23:34:40 +0300

    Update python-profile-checker.py

--- python-profile-checker.py
+++ python-profile-checker.py
@@ -6,8 +6,8 @@ print("Answer by 'yes' or 'no':")
 graduate = input("Have you graduated?: ")
 
 if age < 25 and gpa >= 3.5 and graduate == "yes":
-    print(name,", you are eligible for a.")
+    print(name,", you are eligible for the scholarship offer.")
 elif age < 30 and gpa >= 2.5:
-    print (name, ", you are eligible for an.")
+    print (name, ", you are eligible.")
 else:
     print (name, ", please apply again later.")    
