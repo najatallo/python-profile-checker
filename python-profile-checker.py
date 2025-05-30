f001526e7c3ff0e9b327aa0257839f4c9fe5aaea
Najat <najatallo21@gmail.com>
2025-05-30 21:56:47 +0300

    My first Commit

---
+++ python-profile-checker.py
@@ -0,0 +1,13 @@
+print("Fill in the following information: ")
+name=input("Name: ")
+age=int(input("Age: "))
+gpa= float(input("GPA: "))
+print("Answer by 'yes' or 'no':")
+graduate = input("Have you graduated?: ")
+
+if age < 25 and gpa >= 3.5 and graduate == "yes":
+    print(name,", you are eligible for a.")
+elif age < 30 and gpa >= 2.5:
+    print (name, ", you are eligible for an.")
+else:
+    print (name, ", please apply again later.")    
